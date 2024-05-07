2a5e5642e40566da8c419a73abda9100435518f2
o.le <oleschulz123@gmail.com>
2024-05-07 18:56:54 +0200

    Update JavaDoc

--- src/main/App.java
+++ src/main/App.java
@@ -2,7 +2,7 @@ package main;
 
 /**
  * @author                              o.le
- * @version                             0.4.0
+ * @version                             0.4.1
  * @since                               0.1.0
  */
 class App {}
