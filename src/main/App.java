5af22510b7d85a99a11865a771b4bf921d6c2073
o.le <oleschulz123@gmail.com>
2024-05-07 14:56:44 +0200

    Changed some javadoc

--- src/main/App.java
+++ src/main/App.java
@@ -2,7 +2,7 @@ package main;
 
 /**
  * @author                              o.le
- * @version                             0.3.0
+ * @version                             0.3.1
  * @since                               0.1.0
  */
 class App {}
