41826abd71c5e76d741f651f38cf2837aaa124ca
o.le <oleschulz123@gmail.com>
2024-07-03 10:54:20 +0200

    Adding new behavior pattern

--- src/main/App.java
+++ src/main/App.java
@@ -2,7 +2,7 @@ package main;
 
 /**
  * @author                              o.le
- * @version                             0.13
+ * @version                             0.15
  * @since                               0.1
  */
 class App {}
