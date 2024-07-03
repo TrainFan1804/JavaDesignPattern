1071c1853e12605f65458ab0b93aff9d4db0173e
o.le <oleschulz123@gmail.com>
2024-07-03 14:01:26 +0200

    Adding template pattern

--- src/main/App.java
+++ src/main/App.java
@@ -2,7 +2,7 @@ package main;
 
 /**
  * @author                              o.le
- * @version                             0.16
+ * @version                             0.17
  * @since                               0.1
  */
 class App {}
---
+++ src/main/patterns/behavioralpatterns/template/Client.java
@@ -0,0 +1,19 @@
+package main.patterns.behavioralpatterns.template;
+
+/**
+ * This type represent the client that use a {@link Template}.
+ * 
+ * @author                              o.le
+ * @version                             0.2
+ * @since                               0.17
+ */
+class Client {
+
+    private Template template = new TemplateOne();
+
+    public static void main(String[] args) {
+        
+        Client client = new Client();
+        client.template.templateMethod();
+    }
+}
