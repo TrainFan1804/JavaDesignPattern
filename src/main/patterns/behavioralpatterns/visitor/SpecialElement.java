51a90cd877a4fc02bda130383f4aca8713255cf9
o.le <oleschulz123@gmail.com>
2024-07-03 14:44:22 +0200

    Adding visitor pattern

---
+++ src/main/patterns/behavioralpatterns/visitor/SpecialElement.java
@@ -0,0 +1,18 @@
+package main.patterns.behavioralpatterns.visitor;
+
+/**
+ * This type is a special element that is expand with new features. This class
+ * is inside the old system that want to be expanded.
+ * 
+ * @author                              o.le
+ * @version                             0.3
+ * @since                               0.17
+ */
+class SpecialElement extends Element {
+
+    @Override
+    void accept(Visitor visitor) {
+
+        visitor.visitElement(this);
+    }
+}
---
+++ src/main/patterns/behavioralpatterns/visitor/SpecialVisitorOne.java
@@ -0,0 +1,19 @@
+package main.patterns.behavioralpatterns.visitor;
+
+/**
+ * This type is a special visitor that provide new functionality. This class
+ * is inside the new system. This special visitor provide a method to do
+ * something you couldn't do with the old system without chaning it.
+ * 
+ * @author                              o.le
+ * @version                             0.4
+ * @since                               0.17
+ */
+class SpecialVisitorOne implements Visitor {
+
+    @Override
+    public void visitElement(Element element) {
+     
+        System.out.println(element.x);
+    }
+}
