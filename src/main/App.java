1d3169c29e5bcc085b39de51e115c93008baa3f6
o.le <oleschulz123@gmail.com>
2024-07-04 09:49:38 +0200

    Change version

--- src/main/App.java
+++ src/main/App.java
@@ -2,7 +2,7 @@ package main;
 
 /**
  * @author                              o.le
- * @version                             0.19
+ * @version                             0.20
  * @since                               0.1
  */
 class App {}
