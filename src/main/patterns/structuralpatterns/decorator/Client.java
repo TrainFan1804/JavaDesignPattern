368af7f527bff929aa42e0a97d5e5d52268db552
o.le <oleschulz123@gmail.com>
2024-08-07 09:40:55 +0200

    Update Client class in decorator pattern

--- src/main/patterns/structuralpatterns/decorator/Client.java
+++ src/main/patterns/structuralpatterns/decorator/Client.java
@@ -2,17 +2,10 @@ package main.patterns.structuralpatterns.decorator;
 
 class Client {
 
-    private Component component;
-
-    public Client() {
-
-        Component c = new ConcreteComponent();
-        this.component = new ConcreteDecorator(c); 
-    }
-
     public static void main(String[] args) {
-        
-        Client client = new Client();
-        System.out.println(client.component.operation()); 
+       
+        Component component = new ConcreteComponent();
+        component = new ConcreteDecorator(component);
+        System.out.println(component.operation()); 
     }
 }
