3f6079391bc03fc0efe26ed67849d5b0795a38e3
o.le <oleschulz123@gmail.com>
2024-08-08 13:32:33 +0200

    Update facade pattern

---
+++ src/main/patterns/structuralpatterns/facade/README.md
@@ -0,0 +1,20 @@
+# Facade Pattern
+
+The facade pattern provide a interface for an subsystem. The facade pattern is 
+a **GoF** pattern. 
+
+# Class Description
+
+## Facade
+
+The facade provide controlled and easy acces to the subsystem the facade is
+defining.
+
+# UML
+
+![UML](../../../../../resource/Facade_UML.png)
+
+# Sources
+
+Information: https://en.wikipedia.org/wiki/Facade_pattern 
+Image: https://en.wikipedia.org/wiki/Facade_pattern#/media/File:Example_of_Facade_design_pattern_in_UML.png
--- src/main/patterns/structuralpatterns/facade/subsystem/SubsystemClassOne.java
+++ src/main/patterns/structuralpatterns/facade/subsystem/SubsystemClassOne.java
@@ -1,18 +1,8 @@
 package main.patterns.structuralpatterns.facade.subsystem;
 
-/**
- * This is some random class that simulate a class in a subsystem.
- * 
- * @author                              o.le
- * @version                             0.2
- * @since                               0.11
- */
 class SubsystemClassOne {
 
-    /**
-     * Do something in the subsystem.
-     */
-    void doSomething() {
+    public void doSomething() {
 
         System.out.println("First call from the subsystem.");
     }
